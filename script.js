210bdbeb6e355c7b462a45751c994bf7fbbd6c7b
Stephany <stephanykleen@gmail.com>
2023-03-07 11:50:14 -0800

    Create HTML file and link JS file

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice(){
+  let player =  prompt('Rock, Paper, Scissor?')
+
+}
+console.log(getComputerChoice())
+//let rock = 1
+//let paper = 2
+//let scissors = 3
+//let play = Math.floor(Math.random() * 3) 
